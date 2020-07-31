132524f353d730cee33253153e374fc7d90c089d
Elllin <noto01@naver.com>
2020-07-31 09:16:47 +0900

    Feat : done 일때 스타일 추가

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,13 +1,13 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
 
-function TodoItem() {
+function TodoItem({ id, text, done }) {
   return (
     <TodoItemBlock>
-      <CheckCircle />
-      <Text>어쩌구 저쩌구</Text>
-      <Remove>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove done={done}>
         <MdDelete />
       </Remove>
     </TodoItemBlock>
@@ -15,24 +15,44 @@ function TodoItem() {
 }
 
 const CheckCircle = styled.div`
-  margin-right: 10px;
-  width: 28px;
-  height: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-right: 13px;
+  width: 22px;
+  height: 22px;
   border-radius: 16px;
   border: 1px solid #ced4da;
-  font-size: 24px;
+  font-size: 17px;
+
+  ${({ done }) =>
+    done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
 `;
 
 const Text = styled.div`
   flex: 1;
-  font-size: 21px;
+  font-size: 17px;
   color: #495057;
+  ${({ done }) =>
+    done &&
+    css`
+      color: #ced4da;
+      text-decoration: line-through;
+    `}
 `;
 
 const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   opacity: 0;
   color: #dee2e6;
-  font-size: 24px;
+  font-size: 22px;
   cursor: pointer;
   &:hover {
     color: #ff6b6b;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -5,7 +5,10 @@ import TodoItem from "./TodoItem";
 function TodoList() {
   return (
     <TodoListBlock>
-      <TodoItem />
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
+      <TodoItem text="Context 만들기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} />
     </TodoListBlock>
   );
 }
